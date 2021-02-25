6867f84a94a3d9438755a6f2562fc7d164617c98
Mikael Vaaltola <mvaaltola@gmail.com>
2021-02-25 23:04:08 +0200

    Add temp, vol blocks

--- blocks.h
+++ blocks.h
@@ -1,9 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	10,		0},
-
-	{"", "date '+%d.%m %T'",					1,		0},
+	{"t", "sensors | awk '/Core 0/ {print $3}' | cut -c2-5", 10, 0},
+	{"", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 10, 0},
+	{"/", "cat /sys/class/power_supply/BAT1/capacity 2>/dev/null", 30, 0},
+	{"v", "awk -F'[][]' '/Left:/ { print $2 }' <(amixer sget Master)", 0, 10},
+	{"", "date '+%d.%m %T'", 1,	0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
